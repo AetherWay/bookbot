1520b46aad9526cf1d8abe8de9c47f86b1a9019f
AetherWay <jeffbfrancis80@gmail.com>
2024-12-12 09:44:00 -0700

    add word count function and print Frankenstein word count in main

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ import __main__
 def main():
         with open("books/frankenstein.txt") as f:
                 file_contents = f.read()
+                print(f"Frankenstein has {word_count(file_contents)} words")
+
+def word_count(book_text):
+        return len(book_text.split())
 
 main()
